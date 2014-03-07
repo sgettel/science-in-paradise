8b1ccbe1930f8cd0778cda1438c2ad86a777b957
Bence Béky <zsebkecske@gmail.com>
2014-03-07 16:16:25 -0500

    Add comments.

--- geocoding.py
+++ geocoding.py
@@ -8,11 +8,14 @@ import glob;
 import numpy;
 
 for filename in numpy.sort(glob.glob("data/meeting_list_????_utf8.txt")):
+  # Read JSON file into one large Array.
   print("Reading " + filename);
   with open(filename, "rt") as input:
     meetingArray = json.load(input);
+  # Cycle through all meetings.
   for meetingIndex in range(len(meetingArray)):
     meeting = meetingArray[meetingIndex];
+    # See if there is location information, or if not, at least address.
     if len(meeting["location"]) == 0:
       if len(meeting["address"]) == 0:
         continue;
@@ -20,17 +23,25 @@ for filename in numpy.sort(glob.glob("data/meeting_list_????_utf8.txt")):
         location = meeting["address"];
     else:
       location = meeting["location"];
+    # Quote accented letters and stuff, and prepare URL.
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + urllib.parse.quote(location) + "&sensor=false";
+    # Query Google Maps into a file.
     response = urllib.request.urlretrieve(url);
+    # Read file, again JSON.
     with open(response[0], "rt") as input:
       data = json.load(input);
+    # If there are no results, skip.
     if len(data["results"]) == 0:
       print("No results for " + meeting["address"]);
       continue;
+    # Otherwise, print coordinates.
     latlong = data["results"][0]["geometry"]["location"];
     print("{0:d} results for {1:s}, lat {2:f}, long {3:f}.".format(len(data["results"]), location, latlong["lat"], latlong["lng"]));
+    # Add field to meeting dict.
     meeting["GoogleMapsResponse"] = data;
+  # Save array of dicts of arrays and dicts of arrays and dicts of... into file.
   print("Writing " + filename[:-4] + "_with_GoogleMapsResponse.txt");
   with open(filename[:-4] + "_with_GoogleMapsResponse.txt", "wt") as output:
     json.dump(meetingArray, output);
+  # Clean up tempfiles after processing each year.
   urllib.request.urlcleanup();
--- worldmap.py
+++ worldmap.py
@@ -8,24 +8,31 @@ from matplotlib import pyplot;
 pyplot.ion();
 from mpl_toolkits.basemap import Basemap;
 
+# Parse coordinates into these arrays.
 latArray = [];
 longArray = [];
 
 for filename in numpy.sort(glob.glob("data/meeting_list_????_utf8_with_GoogleMapsResponse.txt")):
+  # Read each year into one large array.
   with open(filename, "rt") as input:
     meetingArray = json.load(input);
   for meetingIndex in range(len(meetingArray)):
+    # Cycle through each meeting, extract coordinates.
     try:
       latlong = meetingArray[meetingIndex]["GoogleMapsResponse"]["results"][0]["geometry"]["location"];
       latArray.append(latlong["lat"]);
       longArray.append(latlong["lng"]);
+    # Do not stress out too much if we don't find anything.
     except KeyError:
       pass;
 
+# Draw projection with coastlines and countries.
+# Do not color continents, otherwise it will always be above scatterplot.
 pyplot.figure();
 m = Basemap(projection="kav7", lon_0=0, resolution="l");
 m.drawmapboundary(fill_color="white");
 m.drawcoastlines();
 m.drawcountries();
+# Transform geographical coordinates into Basemap coordinates, and plot.
 x, y = m(longArray,latArray);
 m.scatter(x, y, marker="o", color="k");
