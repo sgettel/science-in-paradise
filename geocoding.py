3c8e3958c4d188c2f72968e48d2d64e0e3b2434e
Bence Béky <zsebkecske@gmail.com>
2014-03-07 14:58:14 -0500

    Implement GoogleMaps query and insertion to JSON.

--- geocoding.py
+++ geocoding.py
@@ -7,19 +7,34 @@ import json;
 import glob;
 import numpy;
 
-for filename in numpy.sort(glob.glob("meeting_list_????.txt")):
+#for filename in numpy.sort(glob.glob("data/meeting_list_????_utf8.txt")):
+for filename in [
+       'data/meeting_list_2011_utf8.txt',
+       'data/meeting_list_2012_utf8.txt',
+       'data/meeting_list_2013_utf8.txt']:
+  print("Reading " + filename);
   with open(filename, "rt") as input:
-    meetingData = json.load(input, encoding="iso-8859-2");
-  for meeting in meetingData:
-    if len(meeting["address"]) == 0:
-      continue;
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + meeting["address"] + "&sensor=false";
+    meetingArray = json.load(input);
+  for meetingIndex in range(len(meetingArray)):
+    meeting = meetingArray[meetingIndex];
+    if len(meeting["location"]) == 0:
+      if len(meeting["address"]) == 0:
+        continue;
+      else:
+        location = meeting["address"];
+    else:
+      location = meeting["location"];
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + urllib.parse.quote(location) + "&sensor=false";
     response = urllib.request.urlretrieve(url);
     with open(response[0], "rt") as input:
       data = json.load(input);
-      if len(data["results"]) == 0:
-        continue;
-      latlong = data["results"][0]["geometry"]["location"];
-      print(latlong["lat"], latlong["lng"]);
-
-urllib.request.urlcleanup();
+    if len(data["results"]) == 0:
+      print("No results for " + meeting["address"]);
+      continue;
+    latlong = data["results"][0]["geometry"]["location"];
+    print("{0:d} results for {1:s}, lat {2:f}, long {3:f}.".format(len(data["results"]), location, latlong["lat"], latlong["lng"]));
+    meeting["GoogleMapsResponse"] = data;
+  print("Writing " + filename[:-4] + "_with_GoogleMapsResponse.txt");
+  with open(filename[:-4] + "_with_GoogleMapsResponse.txt", "wt") as output:
+    json.dump(meetingArray, output);
+  urllib.request.urlcleanup();
