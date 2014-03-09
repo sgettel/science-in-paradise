3633c4a594a03c30524b005b2299067af8d44e4d
Bence Béky <zsebkecske@gmail.com>
2014-03-09 11:51:21 -0400

    Merge data with geolocation into single JSON file.

--- move_to_one_file.py
+++ move_to_one_file.py
@@ -5,16 +5,17 @@ import json;
 import glob;
 import numpy;
 
-allMeetings = [];
+for suffix, encoding in [[".txt", "iso-8859-2"], ["_utf8_with_GoogleMapsResponse.txt", "utf8"]]:
+  allMeetings = [];
 
-for filename in numpy.sort(glob.glob("data/meeting_list_????.txt")):
-  # Read JSON file into one large Array.
-  print("Reading " + filename);
-  with open(filename, "rt") as input:
-    meetingsThisYear = json.load(input, encoding="iso-8859-2");
-  allMeetings += meetingsThisYear;
+  for filename in numpy.sort(glob.glob("data/meeting_list_????" + suffix)):
+    # Read JSON file into one large Array.
+    print("Reading " + filename);
+    with open(filename, "rt") as input:
+      meetingsThisYear = json.load(input, encoding=encoding);
+    allMeetings += meetingsThisYear;
 
-filename = "data/meeting_list_all.txt";
-print("Writing {0:d} meetings to {1:s}.".format(len(allMeetings), filename));
-with open(filename, "wt") as output:
-  json.dump(allMeetings, output);
+  filename = "data/meeting_list_all" + suffix;
+  print("Writing {0:d} meetings to {1:s}.".format(len(allMeetings), filename));
+  with open(filename, "wt") as output:
+    json.dump(allMeetings, output);
